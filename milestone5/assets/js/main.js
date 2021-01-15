1c023a738dfdd59609863eb78e0fc5ebd4867572
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-01-15 16:41:24 +0100

    Aggiunti 5 attori alla card
    Invocato il methods cast al''interno delle chiamate axios per film e serietv

--- milestone5/assets/js/main.js
+++ milestone5/assets/js/main.js
@@ -1,12 +1,9 @@
-// Milestone 4:
-// Trasformiamo quello che abbiamo fatto fino ad ora in una vera e propria webapp,
-// creando un layout completo simil-Netflix:
-// ● Un header che contiene logo e search bar
-// ● Dopo aver ricercato qualcosa nella searchbar, i risultati appaiono sotto forma
-// di “card” in cui lo sfondo è rappresentato dall’immagine di copertina (consiglio
-// la poster_path con w342)
-// ● Andando con il mouse sopra una card (on hover), appaiono le informazioni
-// aggiuntive già prese nei punti precedenti più la overview
+// Milestone 5 (Opzionale):
+// Partendo da un film o da una serie, richiedere all'API quali sono gli attori che fanno
+// parte del cast aggiungendo alla nostra scheda Film / Serie SOLO i primi 5 restituiti
+// dall’API con Nome e Cognome, e i generi associati al film con questo schema:
+// “Genere 1, Genere 2, …”.
+
 
 let app = new Vue({
     el: '#app',
@@ -35,7 +32,10 @@ let app = new Vue({
                     elemento = Math.ceil((element.vote_average)/2);
                     //console.log(element);
                     this.voto.push(elemento);
-                    //tramite la proprieta id trovo il cast del film
+                    //invoco la funzione cast e la salvo in una variabile
+                    const nomiAttori = this.cast(element.id)
+                    //attraverso il vue set aggiungo una proprietà all'oggetto
+                    Vue.set(element, 'attori', nomiAttori)
                 });
                 //console.log(Math.ceil((this.risultati[0].vote_average)/2));
             });
@@ -48,25 +48,35 @@ let app = new Vue({
                  //console.log(response.data.results);
                  this.risultatiSerie = response.data.results
                  this.risultatiSerie.forEach(element => {
-                     element = Math.ceil((element.vote_average)/2);
+                     elemento = Math.ceil((element.vote_average)/2);
                      //console.log(element);
                      this.votoSerie.push(element);
+                     //invoco la funzione cast e la salvo in una variabile
+                    const nomiAttori = this.cast(element.id)
+                    //attraverso il vue set aggiungo una proprietà all'oggetto
+                    Vue.set(element, 'attori', nomiAttori)
                     });
                     //console.log(Math.ceil((this.risultati[0].vote_average)/2));
  
              });
 
-
              
         },
-  
-        cast(filmId){
+        
+       /** ## funzione cast 
+        * 
+        * Questa funzione fa una chiamata ajax per trovare il cast tramite Id
+        * @param {number}filmId passare alla funzione l'Id del film
+        * return array
+        * 
+        * */ 
+            cast(filmId){
             let attori = [];
             let attoriPrincipali= [];
             axios.get(("https://api.themoviedb.org/3/movie/") + (filmId) + ("/credits?api_key=fbebf38b6276068d6e0065d2377875c0"))
             .then(response =>{
                 //verifico lo stato della risposta
-                console.log(response);
+                //console.log(response);
                 const cast=(response.data.cast);
                 //console.log(cast);
                 cast.forEach(element => {
@@ -77,18 +87,15 @@ let app = new Vue({
                 for (let index = 0; index < 5; index++) {
                     attoriPrincipali.push(attori[index]);
                 }
-                //console.log(attoriPrincipali);
-            
+                
                 
             })
             .catch(error=>{
                 console.log(error);
             })
-            .finally(()=>{
-                console.log(attoriPrincipali);
-                return attoriPrincipali;
-
-            })
+            //console.log(attoriPrincipali);
+            return attoriPrincipali;
+       
         }
         
     }
