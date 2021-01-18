6220819509d0b6381a18e876036ea2a24d8f377f
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-01-18 12:36:44 +0100

    Utilizzato map per associare i generi al numero e poi aggiunti come prop
    Utilizzato map per associare i generi al numero e poi aggiunti come proprietà

--- milestone5/assets/js/main.js
+++ milestone5/assets/js/main.js
@@ -37,10 +37,59 @@ let app = new Vue({
                     //attraverso il vue set aggiungo una proprietà all'oggetto
                     Vue.set(element, 'attori', nomiAttori)
 
+                    //creo una variabile in cui salvo i generi
                     const generi = element.genre_ids
-                    console.log(generi);
+                    //console.log(generi);
+               
+                    //utilizzando map sotituisco i valori numeri in stringhe contenenti il genere
+                    const newGeneri = generi.map((numero, index, array) => {
+                    //console.log(index);
+                    if(numero == '28') {
+                        return 'Action';
+                    }else if(numero == "12"){
+                        return 'Adventure'
+                    }else if(numero == "16"){
+                        return 'Animation'
+                    }else if(numero == "35"){
+                        return 'Comedy'
+                    }else if(numero == "80"){
+                        return 'Crime'
+                    }else if(numero == "99"){
+                        return 'Documentary'
+                    }else if(numero == "18"){
+                        return 'Drama'
+                    }else if(numero == "10751"){
+                        return 'Family'
+                    }else if(numero == "14"){
+                        return 'Fantasy'
+                    }else if(numero == "36"){
+                        return 'History'
+                    }else if(numero == "27"){
+                        return 'Horror'
+                    }else if(numero == "10402"){
+                        return 'Music'
+                    }else if(numero == "9648"){
+                        return 'Mystery'
+                    }else if(numero == "10749"){
+                        return 'Romance'
+                    }else if(numero == "878"){
+                        return 'Science Fiction'
+                    }else if(numero == "10770"){
+                        return 'TV Movie'
+                    }else if(numero == "53"){
+                        return 'Thriller'
+                    }else if(numero == "10752"){
+                        return 'War'
+                    }else if(numero == "37"){
+                        return 'Western'
+                    }
+                    return numero;
+                    });
+                    // aggiungo la proprietà generi all'oggetto
+                    Vue.set(element, 'generi', newGeneri)
+                    
                 });
-                //console.log(Math.ceil((this.risultati[0].vote_average)/2));
+            
             });
 
              //concatenando il link con this.film posso cercare il film che scrivo nell'input
@@ -58,6 +107,51 @@ let app = new Vue({
                     const nomiAttori = this.cast(element.id)
                     //attraverso il vue set aggiungo una proprietà all'oggetto
                     Vue.set(element, 'attori', nomiAttori)
+
+                    //creo una variabile in cui salvo i generi
+                    const generi = element.genre_ids
+                    //console.log(generi);
+               
+                    //utilizzando map sotituisco i valori numeri in stringhe contenenti il genere
+                    const newGeneri = generi.map((numero, index, array) => {
+                    //console.log(index);
+                    if(numero == '10759') {
+                        return 'Action & Adventure';
+                    }else if(numero == "16"){
+                        return 'Animation'
+                    }else if(numero == "35"){
+                        return 'Comedy'
+                    }else if(numero == "80"){
+                        return 'Crime'
+                    }else if(numero == "99"){
+                        return 'Documentary'
+                    }else if(numero == "18"){
+                        return 'Drama'
+                    }else if(numero == "10751"){
+                        return 'Family'
+                    }else if(numero == "10762"){
+                        return 'Kids'
+                    }else if(numero == "9648"){
+                        return 'Mystery'
+                    }else if(numero == "10763"){
+                        return 'News'
+                    }else if(numero == "10764"){
+                        return 'Reality'
+                    }else if(numero == "10765"){
+                        return 'Sci-Fi & Fantasy'
+                    }else if(numero == "10766"){
+                        return 'Soap'
+                    }else if(numero == "10767"){
+                        return 'Talk'
+                    }else if(numero == "10768"){
+                        return 'War & Politics'
+                    }else if(numero == "37"){
+                        return 'Western'
+                    }
+                    return numero;
+                    });
+                    // aggiungo la proprietà generi all'oggetto
+                    Vue.set(element, 'generi', newGeneri)
                     });
                     //console.log(Math.ceil((this.risultati[0].vote_average)/2));
  
@@ -99,8 +193,31 @@ let app = new Vue({
             //console.log(attoriPrincipali);
             return attoriPrincipali;
        
-        }
+        },
         
+        //Utilizzata la chiamata axios per trovare i numeri associati al genere
+        // genre(){
+        //     axios.get("https://api.themoviedb.org/3/genre/movie/list?api_key=fbebf38b6276068d6e0065d2377875c0&language=en-US")
+        //     .then(response =>{
+        //         //verifico lo stato della risposta
+        //         //console.log(response);
+        //         let listGeneri = (response.data)
+        //         //console.log(listGeneri);
+        //         return listGeneri;      
+        //     })
+        // }
+
+        //Utilizzata la chiamata axios per trovare i numeri associati al genere
+        // genreSerie(){
+        //     axios.get("https://api.themoviedb.org/3/genre/tv/list?api_key=fbebf38b6276068d6e0065d2377875c0&language=en-US")
+        //     .then(response =>{
+        //         //verifico lo stato della risposta
+        //         //console.log(response);
+        //         let listGeneri = (response.data)
+        //         //console.log(listGeneri);
+        //         return listGeneri;      
+        //     })
+        // }
     }
 });
 
